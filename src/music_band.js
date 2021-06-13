a7e9e7545e17a550153de0b74d55c4c9ba502cee
sophia <1786728683@qq.com>
2021-06-13 11:06:36 +0000

    add band and cloud3

--- src/music_band.js
+++ src/music_band.js
@@ -1,27 +1,27 @@
 import React from 'react'; 
 import './music_bandStyle.css';
-export function Cloud1({}) {
+export function Cloud1() {
 	return <>
 		<div className='cloud1'>
 		</div>
 	</>
 }
 
-export function Cloud2({}) {
+export function Cloud2() {
 	return <>
 		<div className='cloud2'>
 		</div>
 	</>
 }
 
-export function Cloud3({}) {
+export function Cloud3() {
 	return <>
 		<div className='cloud3'>
 		</div>
 	</>
 }
 
-export function Myband ({}) {
+export function Myband () {
 	const [pause, setpause] = React.useState(false);
 	return <>
 	{ pause &&
